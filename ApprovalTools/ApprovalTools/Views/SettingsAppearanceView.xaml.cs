a2ab6a600ee16fa64193d6bb73688e304d31e428
Ilya Margolin <imargolin@luware.net>
2015-09-22 13:48:58 +0300

    Use ModernUI to enable dark theme

---
+++ ApprovalTools/ApprovalTools/Views/SettingsAppearanceView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ApprovalTools.Approve.Views
+{
+    /// <summary>
+    /// Interaction logic for SettingsAppearance.xaml
+    /// </summary>
+    public partial class SettingsAppearanceView : UserControl
+    {
+        public SettingsAppearanceView()
+        {
+            InitializeComponent();
+        }
+    }
+}
