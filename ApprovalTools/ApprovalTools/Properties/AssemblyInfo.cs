1b292c37a90b3438a295a1ba9aa9bda34b954d15
Ilya Margolin <imargolin@luware.net>
2015-09-23 14:13:31 +0300

    Add Squirrel: First version

--- ApprovalTools/ApprovalTools/Properties/AssemblyInfo.cs
+++ ApprovalTools/ApprovalTools/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ApprovalTools.Approve")]
+[assembly: AssemblyTitle("Approval Tools")]
 [assembly: AssemblyDescription("Works with Approvals library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ApprovalTools.Approve")]
+[assembly: AssemblyCompany("Ilya inc.")]
+[assembly: AssemblyProduct("Approval Tools")]
 [assembly: AssemblyCopyright("Copyright © Ilya 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
